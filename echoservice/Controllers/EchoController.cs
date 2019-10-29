939613aa0deb85add054ac4d0aa8bca6bf321415
Max Sivkov <maxsivkov@gmail.com>
2019-10-29 16:19:38 +0200

    initial

---
+++ echoservice/Controllers/EchoController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace echoservice.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EchoController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet("headers")]
+        public JsonResult Get()
+        {
+            //return new string[] { "value1", "value2" };
+            NameValueCollection headers = new NameValueCollection();
+            foreach (var h in Request.Headers)
+                headers.Add(h.Key, h.Value);
+            return new JsonResult(new
+            {
+                headers = Request.Headers,
+                query = Request.QueryString
+            });
+        }
+        [HttpPost("file")]
+        public JsonResult Post(IFormFile file)
+        {
+            return new JsonResult(new
+            {
+                filename = file.FileName,
+                filesize = file.Length
+            });
+        }
+    }
+}
---
+++ echoservice/Controllers/ValuesController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace echoservice.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public JsonResult Get()
+        {
+            //return new string[] { "value1", "value2" };
+            NameValueCollection headers = new NameValueCollection();
+            foreach (var h in Request.Headers)
+                headers.Add(h.Key, h.Value);
+            return new JsonResult(new
+            {
+                headers = Request.Headers
+            });
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
